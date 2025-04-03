23bc95b0f2f37cbe09872f9336e1217fb2c783b5
jamshidbekman <jamshidbekmansurov01@gmail.com>
2025-04-03 21:43:14 +0500

    projects module: some updates

--- src/modules/projects/projects.service.ts
+++ src/modules/projects/projects.service.ts
@@ -4,23 +4,5 @@ import { UpdateProjectDto } from './dto/update-project.dto';
 
 @Injectable()
 export class ProjectsService {
-  create(createProjectDto: CreateProjectDto) {
-    return 'This action adds a new project';
-  }
-
-  findAll() {
-    return `This action returns all projects`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} project`;
-  }
-
-  update(id: number, updateProjectDto: UpdateProjectDto) {
-    return `This action updates a #${id} project`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} project`;
-  }
+  
 }
